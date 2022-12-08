e6a6b1d73a1cf506b7ddeb55867fdc6c172899fa
raelum <100334527+raelum@users.noreply.github.com>
2022-12-07 23:44:01 -0800

    Remove 'kj' for escape and shorten timeout length

--- config/nvim.symlink/lua/raelum/core/keymaps.lua
+++ config/nvim.symlink/lua/raelum/core/keymaps.lua
@@ -2,9 +2,11 @@ local keymap = vim.keymap
 
 vim.g.mapleader = ' '
 
--- Easier escape shortcuts
+-- Allow 'jk' to escape while in insert mode
+-- This causes a delay when typing 'j' however, 'j' is the least likely keyboard
+-- home letter to appear in the English language. :)
+-- https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
 keymap.set('i', 'jk', '<ESC>')
-keymap.set('i', 'kj', '<ESC>')
 
 -- Don't copy to register when deleting a character
 --keymap.set('n', 'x', '"_x')
