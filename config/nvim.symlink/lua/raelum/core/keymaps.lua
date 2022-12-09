4f6793b45e67158c22519cd7ef15385f27f433f9
raelum <100334527+raelum@users.noreply.github.com>
2022-12-09 13:32:27 -0800

    Center view when searching or scrolling through code

--- config/nvim.symlink/lua/raelum/core/keymaps.lua
+++ config/nvim.symlink/lua/raelum/core/keymaps.lua
@@ -8,5 +8,18 @@ keymap.set('n', '<Esc>', ':noh<CR>', { silent = true })
 -- Write file shortcut (uses update so only writes when there are changes)
 keymap.set('n', '<Leader>w', ':update<CR>')
 
+-- Tab navigation
 keymap.set('n', '<Leader>t', ':tabnext<CR>')
 keymap.set('n', '<Leader>T', ':tabprevious<CR>')
+
+-- Center view when searching
+keymap.set('n', 'n', 'nzz')
+keymap.set('n', 'N', 'Nzz')
+keymap.set('n', '*', '*zz')
+keymap.set('n', '#', '#zz')
+
+-- Center view when scrolling through code
+keymap.set('n', 'j', 'jzz')
+keymap.set('n', 'k', 'kzz')
+keymap.set('n', '<C-d>', '<C-d>zz')
+keymap.set('n', '<C-u>', '<C-u>zz')
